683611d2ab5c27917a7d161648c8760a585eed4e
Oleksandr Bozhuk <oleksandr.bozhuk@namecheap.com>
2022-11-16 12:59:31 +0100

    Add spring boot skeleton

---
+++ CItest/src/test/java/com/olekb/CItest/CItestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olekb.CItest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CItestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
